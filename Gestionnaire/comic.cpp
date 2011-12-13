2b3e939377c5d2ac9b79cabd43afa3d6f74102ac
Marc Hayet <murten.64@gmail.com>
2011-12-13 16:51:51 +0100

    correction manga et commic

--- Gestionnaire/comic.cpp
+++ Gestionnaire/comic.cpp
@@ -8,6 +8,83 @@ Comic::~Comic()
 {
 }
 
+void Comic::ajouter()
+{
+    std::string file;
+    std::cout<<"Ajout du Comic:"<<std::endl;
+
+    file = "../Elements/";
+    file += _title;
+    file += ".txt";
+
+    std::ofstream Ajout(file.c_str());
+    if (Ajout)
+    {
+        Ajout << "Titre: ";
+        Ajout << _title <<std::endl;
+        Ajout << "Auteur: ";
+        Ajout << _author <<std::endl;
+        Ajout << "Editeur: ";
+        Ajout << _editor <<std::endl;
+    }
+    else
+    {
+        std::cout<<"ERREUR: Impossible de créer le fichier."<<std::endl;
+    }
+
+    std::cout<<"Veuillez appuyer sur enter pour continuer."<<std::endl;
+    getchar();
+    std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+}
+
+void Comic::modifier()
+{
+    std::string name, author, editor,  file, file2;
+
+    std::cout<<"Modification du manga:"<<std::endl;
+    std::cout<<"Veuillez entrer le nom du manga a modifier:";
+    std::cin>>name;
+    std::cout<<std::endl;
+
+    std::cout<<"Veuillez entrer le nom du manga:";
+    std::cin>>name;
+    std::cout<<std::endl;
+    std::cout<<"Veuillez entrer le nom de l'auteur:";
+    std::cin>>author;
+    std::cout<<std::endl;
+    std::cout<<"Veuillez entrer le nom de l'editeur:";
+    std::cin>>editor;
+    std::cout<<std::endl;
+
+    file = " rm ../Elements/";
+    file += name;
+    file += ".txt";
+
+    file2 = "../Elements/";
+    file2 += name;
+    file2 += ".txt";
+
+    system(file.c_str());
+    std::ofstream Ajout(file2.c_str());
+    if (Ajout)
+    {
+        Ajout << "Titre: ";
+        Ajout << name  <<std::endl;
+        Ajout << "Auteur: ";
+        Ajout << author <<std::endl;
+        Ajout << "Editeur: ";
+        Ajout << editor <<std::endl;
+    }
+    else
+    {
+        std::cout<<"ERREUR: Impossible de modifier le fichier."<<std::endl;
+    }
+
+    std::cout<<"Veuillez appuyer sur enter pour continuer."<<std::endl;
+    getchar();
+    std::cin.ignore(std::numeric_limits<int>::max(), '\n');
+}
+
 void Comic::initialisation()
 {
     std::cout<<"Veuillez entrer le titre du comic : ";
